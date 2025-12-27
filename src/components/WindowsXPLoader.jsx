82658245c2253a7c0e0e8369b4c714711475d97a
ebarinf <eduardo.barrientos.inf@gmail.com>
2025-12-27 14:28:10 -0300

    barra de carga lista

---
+++ src/components/WindowsXPLoader.jsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function WindowsXPLoader() {
+    const [loading, setLoading] = useState(true);
+
+    // useEffect(() => {
+    //     const timer = setTimeout(() => {
+    //         setLoading(false);
+    //     }, 3000); // 3 seconds
+
+    //     return () => clearTimeout(timer);
+    // }, []);
+
+    if (loading) {
+        return (
+            <div
+                style={{
+                    background: "black",
+                    color: "white",
+                    height: "100vh",
+                    width: "100vw",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: "20px",
+                }}
+            >
+                <div className="flex flex-col items-start">
+                    <div className="h-[150px] ml-38">
+                        <img
+                            src="/icons/windows_xp_big.png"
+                            alt="Windows XP icon"
+                            width={180}
+                            height={180}
+                        />
+                    </div>
+
+                    <div className="font-xp text-2xl flex flex-row text-[90px] h-[90px]">
+                        Eduardo
+                        <div className="text-[#D04A28] font-xp text-[70px]">
+                            xp
+                        </div>
+                    </div>
+                    <div className="italic font-xp text-[35px] ml-4">
+                        Web Developer
+                    </div>
+                </div>
+
+                {/* Fake loading bar */}
+                <div className="xp-loader relative">
+                    <div className="bar" />
+                    <div className="bar" />
+                    <div className="bar" />
+                </div>
+
+                <style jsx>{`
+                    @keyframes loader {
+                        0% {
+                            transform: translateX(-60px);
+                        }
+                        100% {
+                            transform: translateX(200px);
+                        }
+                    }
+                `}</style>
+            </div>
+        );
+    }
+}
